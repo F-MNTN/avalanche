6e9c777c68584d5701625e40fae544ca8c8328dc
F-MNTN <felixberger@chello.at>
2026-05-26 00:55:15 +0200

    minor tweaks to aether

---
+++ hosts/aether/default.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/common
+    # ../../modules/desktop # Uncomment if it's a desktop
+    inputs.noctalia.nixosModules.default
+  ];
+
+  networking.hostName = "aether";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = false;
+  services.printing.enable = true;
+
+  # --- Home Manager Bridge ---
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs; };
+    backupFileExtension = "bak";
+    users.mntn = import ../../home/hosts/aether.nix;
+  };
+
+  system.stateVersion = "25.11"; # Adjust to your current NixOS version
+}
